f8c9057ef81b6932f2b4295f3d843194eda15510
Valentin <milhen08@gmail.com>
2017-06-25 15:18:12 +0300

    Update yadisk-tree.hpp

--- include/yadisk/ops/yadisk-tree.hpp
+++ include/yadisk/ops/yadisk-tree.hpp
@@ -1,35 +1,33 @@
 #ifndef TREE_HPP
 #define TREE_HPP
 
-#include "Client.hpp"
-#include "Tree.hpp"
-#include "url/path.hpp"
+#include <yadisk/ops/Client.hpp>
+#include <yadisk/ops/Tree.hpp>
+#include <url/path.hpp>
 
-//using yadisk::Client;
-//using yadisk::ops::Tree;
+using yadisk::Client;
+using yadisk::ops::Tree;
  
 namespace yadisk 
 {
-namespace ops {
- 
-/**
-Ïîëó÷åíèå ñëåïêà äèñêà
-\param[in] ññûëêà íà client-yadisk
-\param[in] ïóòü
-\return âîçâðàùàåò äåðåâî - ñëåïîê äèñêà
-**/
-  Tree tree(Client& client, path home = '/') ;
+ namespace ops 
+ {
  
  /**
-Äîáàâëåíèå äàííûõ
-\param[in] óêàçàòåëü íà äåðåâî
-\param[in] ïóòü
-**/ 
- 
- //void recursive_add(Tree& input, string path, yadisk::ops::Node* parent = NULL)
- 
+ Получение слепка диска
+ \param[in] ссылка на client-yadisk
+ \param[in] путь
+ \return возвращает дерево - слепок диска
+ **/
   
-}
-}
+  Tree tree(Client& client, path home = '/') ;
 
+ /**
+ Добавление данных
+ \param[in] указатель на дерево
+ \param[in] путь
+ **/ 
+  void recursive_add(Tree& input, string path, yadisk::ops::Node* parent = NULL)
+ }
+}
 #endif
