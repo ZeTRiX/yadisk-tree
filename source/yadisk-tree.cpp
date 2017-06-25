4a90441092f507ea391192f1127508c2b02c1d4d
Valentin <milhen08@gmail.com>
2017-06-25 14:10:22 +0300

    Update yadisk-tree.cpp

--- source/yadisk-tree.cpp
+++ source/yadisk-tree.cpp
@@ -1,32 +1,38 @@
-using yadisk::ops::Resource;
-using yadisk::ops::Tree;
-using yadisk::Client;
-using std::string;
+using namespace std;
 
 #include "yadisk/ops/yadisk-tree.hpp"
 
 
-auto ops::tree(Client& client, path home = '/') -> Tree&
-{
-  Tree* client_tree = new Tree;
- 
-recursive_add(client_tree, "/");
-return *client_tree;
-}
-	
-void recursive_add(Tree& input, string path, yadisk::ops::Node* parent = NULL)
-{
-	json data = client.info();
-	Resource rsc(data);
+namespace yadisk{
+{ 
 
-	yadisk::ops::Node* node = input.add_child(rsc, parent);
+	namespace ops
 
-	for (int32_t i = 0; i < data["_embedded"]["total"].get<int32_t>(); i++)
-		if (data["_embedded"]["items"][i]["type"].get<string>() == "dir") {
-			if (path[path.size() - 1] == '/')
-				recursive_add(input, path + data["_embedded"]["items"][i]["name"].get<string>(), node);
-			else
-				recursive_add(input, path + "/" + data["_embedded"]["items"][i]["name"].get<string>(), node);
+	{	
+		auto tree(Client& client, path home = '/') -> Tree&
+		{
+		  Tree* client_tree = new Tree;
+
+		recursive_add(client_tree, "/");
+		return *client_tree;
 		}
 
+		void recursive_add(Tree& input, string path, Node* parent = NULL)
+		{
+			json data = client.info();
+			Resource rsc(data);
+
+			Node* node = input.add_child(rsc, parent);
+
+			for (int32_t i = 0; i < data["_embedded"]["total"].get<int32_t>(); i++)
+				if (data["_embedded"]["items"][i]["type"].get<string>() == "dir") {
+					if (path[path.size() - 1] == '/')
+						recursive_add(input, path + data["_embedded"]["items"][i]["name"].get<string>(), node);
+					else
+						recursive_add(input, path + "/" + data["_embedded"]["items"][i]["name"].get<string>(), node);
+				}
+
+		}
+	}
+	
 }
